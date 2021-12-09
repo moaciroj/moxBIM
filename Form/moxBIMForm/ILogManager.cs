c7866b68f424beb49d3d3d643a45982b3d8dacd5
Moacir de Oliveira Junior <moaciroj@hotmail.com>
2021-12-08 21:40:30 -0300

    Hoje

--- Form/moxBIMForm/ILogManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-public interface ILogManager
-{
-    void LogInformation(string message);
-    void LogWarning(string message);
-    void LogDebug(string message);
-    void LogError(string message);
-}
